3e702bf082db2617cc92ade3f757ccea7ceb716e
Saiteja Reddy <saitejareddy9908@gmail.com>
2025-11-16 07:02:20 +0000

    google sign const firebaseConfig = {   apiKey: "AIzaSyC5nGoafcSVszhFE2B

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-9946265638-eed2a",
-  "appId": "1:141353577770:web:e6f8c8e4c344c75ad94253",
-  "apiKey": "AIzaSyC5nGoafcSVszhFE2BPiIOIPTc0DFItHww",
-  "authDomain": "studio-9946265638-eed2a.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "141353577770"
+  apiKey: "AIzaSyC5nGoafcSVszhFE2BPiIOIPTc0DFItHww",
+  authDomain: "studio-9946265638-eed2a.firebaseapp.com",
+  projectId: "studio-9946265638-eed2a",
+  storageBucket: "studio-9946265638-eed2a.firebasestorage.app",
+  messagingSenderId: "141353577770",
+  appId: "1:141353577770:web:e6f8c8e4c344c75ad94253"
 };
